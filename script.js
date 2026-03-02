0fb1d2c2323c1b7851d05d0c27dbf9f88e9ed123
America <AMericah8@nycstudents.net>
2026-03-02 13:53:55 -0500

    done

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-var key = "";
+var key = "Review";
 
 
 // SAVE
 document.querySelector(".save").onclick = function() {
-
+    var value = document.querySelector("#saveValue").value;
+    localStorage.setItem(key, value);
 };
 
 
 // LOAD
 document.querySelector(".load").onclick = function() {
-
+    var value = localStorage.getItem(key);
+    document.querySelector("#loadValue").value = value;
 };
 
 // LOAD
 document.querySelector(".update").onclick = function() {
-
+    var value = document.querySelector("#updateValue").value;
+    localStorage.setItem(key, value);
 };
 
 // DELETE
 document.querySelector(".delete").onclick = function() {
-
+    localStorage.removeItem(key);
 };
